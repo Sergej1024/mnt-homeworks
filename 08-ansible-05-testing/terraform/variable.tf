49cfabbcbc35c1254b3c1090574addd51fb45ddd
Розум Сергей <sergej_r@list.ru>
2022-08-24 16:37:46 +0500

    Update 8.5

---
+++ 08-ansible-05-testing/terraform/variable.tf
@@ -0,0 +1,16 @@
+variable "YC_TOKEN" {
+  default = "AQAAAAACfG8AAATuwfHJTfeY0kfqjb2D5fkWJfM"
+}
+
+variable "YC_CLOUD_ID" {
+  default = "b1g3839rsq4g6pkhp2e7"
+}
+
+
+variable "YC_FOLDER_ID" {
+  default = "b1gaa80uc55olksp2vtt"
+}
+
+variable "YC_ZONE" {
+  default = "ru-central1-a"
+}
