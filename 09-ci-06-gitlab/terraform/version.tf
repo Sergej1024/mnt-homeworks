9908eb29596f3c012fc2eca0506bee64f4804846
Розум Сергей <sergej_r@list.ru>
2022-09-30 13:49:48 +0500

    Update 9.6

--- 09-ci-06-gitlab/terraform/version.tf
+++ 09-ci-06-gitlab/terraform/version.tf
@@ -5,4 +5,16 @@ terraform {
     }
   }
   required_version = ">= 0.13"
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "backet-rse"
+    region     = "ru-central1"
+    key        = ".terraform/terraform.tfstate"
+    var.access_key = "YCAJEwQdANa_nagUkLZz6HmX8"
+    var.secret_key = "YCO9N6WhuVCz0mCzRQRInrLR0IYq9gj4Cl1GdSA1"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
